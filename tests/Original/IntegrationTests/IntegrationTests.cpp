8b98f11880ea04bfe56fbdb06b55573c1de6d087
Paul Aphivantrakul <aphivantrakul@gmail.com>
2022-09-28 11:16:46 +0700

    Ensure that files in /test/Original/IntegrationTests match that of Bytecoin's
    - files in the following folders also match
      * /test/Original/data
      * /test/Original/Difficulty
      * /test/Original/Hash
      * /test/Original/IntegrationTestLib
    - build succeeds

--- tests/Original/IntegrationTests/IntegrationTests.cpp
+++ tests/Original/IntegrationTests/IntegrationTests.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2011-2016 The Cryptonote developers
+// Copyright (c) 2011-2017 The Cryptonote developers, The Bytecoin developers
 // Copyright (c) 2018-2022 The Cash2 developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
@@ -149,7 +149,7 @@ public:
 
 
 TEST_F(IntegrationTest, Wallet2Wallet) {
-  const uint64_t FEE = 1000000;
+  //const uint64_t FEE = 1000000;
 
   launchTestnet(2);
 
